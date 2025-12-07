7a14afc6f7f99d6c4830b9cd81b70da3c651193e
Robert Cernak <robocernak@gmail.com>
2025-12-07 16:34:59 +0100

    added article to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,10 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <div id="hero" className="hero">
+      <div className="article">
+        <h3>Nové CD</h3>
+        <p>Čoskoro sa môžete tešiť na nové CD.</p>
+      </div>
       <img src={coverImage} />
     </div>
   );
