645f937266fd269de5dfd5c0d47cb34f1b6deee4
Robert Cernak <robocernak@gmail.com>
2025-12-07 17:30:16 +0100

    changing hero articles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,56 @@
-import coverImage from "../../public/cover.jpg";
+import React, { useState, useEffect } from "react";
+import coverImage from "../../public/cover1.jpg";
+import coverImage2 from "../../public/cover2.jpg";
+import coverImage3 from "../../public/cover3.jpg";
 import "./Hero.css";
 
 export default function Hero() {
-  return (
-    <div id="hero" className="hero">
-      <div className="article">
-        <h3>Nové CD</h3>
-        <p>Čoskoro sa môžete tešiť na nové CD.</p>
+  const divs = [
+    <a href="#video">
+      <div className="article dark">
+        <h3>Moja tvorba</h3>
+        <p>Pozrite si niečo z mojej tvorby</p>
       </div>
       <img src={coverImage} />
+    </a>,
+
+    <a href="#music">
+      <div className="article dark">
+        <h3>CD Žalmy</h3>
+        <p>Vypočujte si moje aktuálne CD</p>
+      </div>
+      <img src={coverImage2} />
+    </a>,
+
+    <>
+      <div className="article light">
+        <h3>Nové CD</h3>
+        <p>Čoskoro sa môžete tešiť na moje nové CD</p>
+      </div>
+      <img src={coverImage3} />
+    </>,
+  ];
+
+  const [index, setIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);               // start fade-out
+      setTimeout(() => {
+        setIndex((prev) => (prev + 1) % divs.length);
+        setFade(true);              // fade-in new div
+      }, 500); // match fade-out duration
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={`fade-wrapper ${fade ? "fade-in" : "fade-out"}`}>
+      <div id="hero" className="hero">
+        {divs[index]}
+      </div>
     </div>
   );
 }
