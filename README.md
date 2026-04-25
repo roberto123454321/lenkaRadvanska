de889c395b5a201eea4052840b083805ae8a941e
Robert Cernak <robert.cernak@cat.com>
2026-04-25 21:23:55 +0200

    update bio section

--- README.md
+++ README.md
@@ -40,7 +40,6 @@ firebase deploy
 ```
 ## TODO:
 
-- update bio
 - apple music icon and link
 - add section SHOP
 - domena as a last step. info
--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,89 +5,32 @@ export default function Bio() {
     <div id="bio" className="lighter-background section-padding">
       <div className="bio">
         <p>
-          Lenka Radvanská pochádza z Kendíc, vyštudovala učiteľstvo hudobného
-          umenia v kombinácii s biológiou. Neobvyklé spojenie, ktoré však Lenku
-          výstižne charakterizuje. K učiteľstvu ju priviedli aj spomienky z
-          detstva. S kamarátkami z dediny sa často hrávali na učiteľky a už
-          počas dospievania ju neopúšťala túžba byť s deťmi a mladými. Zároveň
-          mala vždy blízko k biológii, pretože ju fascinoval život a to, ako
-          všetko v človeku funguje. Na druhej strane nadovšetko miluje hudbu.
-          Snažila sa preto nájsť kompromis, ktorý by bol správnou voľbou.
+          Mgr. Lenka Radvanská, PhD. vyštudovala učiteľstvo hudobného umenia a biológie na Prešovskej univerzite v Prešove. Neskôr si svoje hudobné vedomosti rozšírila veľkým doktorátom z Didaktiky hudby na pôde Katedry hudby Katolíckej univerzity v Ružomberku. V oblasti súčasnej kresťanskej hudby sa podieľala na tvorbe hymny Campfestu Som nový (2017), bola súčasťou CD nosičov spoločenstva MaranaTha v Prešove, kde prispela aj autorskými piesňami Vydýchnem a Láska, láska (2019). Ako vokalistka spolupracovala s kresťanskými hudobnými kapelami Timothy, LCH live, s projektom Godzone, či hudobníkmi Dominikou Gurbaľovou a Petrom Křemenom. V roku 2020 spolu s kapelou nahrala debutový album Žalmy.
         </p>
-
-        <h3>MILOVNÍČKA ŽALMOV</h3>
 
         <p>
-          Lenka sa odmalička venuje spevu, ako deväťročnú ju zaujal zbor vo
-          farnosti. V trinástich začala spievať žalmy v kostole, a to najmä
-          počas Veľkej noci na Bielu sobotu. S myšlienkou spracovania žalmov vo
-          vlastnom hudobnom prevedení sa pohrávala často. Popri každoročnom
-          putovaní do Gaboltova s bratmi augustiniánmi pár responzóriových
-          žalmov či žalmových piesní zhudobnila s gitarou. „Počas
-          vysokoškolského štúdia som túžila dať niektoré žalmy do ucelenejšej
-          podoby, osloviť hudobníkov, ktorí by mi pomohli,“ spomína Lenka. Nie
-          je síce profesionálna speváčka, ale počas písania prác do školy
-          premýšľala nad tým, že by žalmy rada počúvala v novej verzii, v
-          džezovom šate. Cez hudobné aplikácie počúva rôzne albumy, „ale chýbali
-          mi žalmy vhodné na počúvanie v rámci voľného času aj po slovensky. To
-          pre mňa bolo popudom ísť za tým, čo prebúdza moje srdce a čo robím
-          veľmi rada“.
+          Božie slovo a špeciálne Kniha žalmov má v mojom živote dôležité miesto. Verím, že cez Slovo sa nám Boh sám dáva spoznať osobným, živým spôsobom. V žalmoch sú ukryté všetky emócie, s ktorými sa človek môže stretnúť. Pomáhajú mi pochopiť, prijať, spracovať životné okolnosti a kráčať ďalej.
         </p>
 
-        <h3>SPLNILA SI SEN</h3>
+        <ul className="bio-links">
+          <li>
+            <a href="https://mojamuzika.dennikn.sk/rozhovory/tuzim-po-tom-aby-zalmove-texty-zazneli-v-kaviarnach-a-baroch-hovori-lenka-bednarova">
+            <h4>Moja Muzika</h4>
+            Túžim po tom, aby žalmové texty zazneli v kaviarňach a baroch, hovorí Lenka Bednárová, ktorá žalmy zhudobňuje do jazzovej podoby</a>
+          </li>
 
-        <p>
-          Lenka je skvelým príkladom toho, že kde je vôľa, tam je i cesta. Svoju
-          myšlienku túžila zrealizovať, a tak oslovovala hudobníkov, rozprávala
-          im o svojom koncepte, až napokon začali na albume žalmov spolu
-          pracovať. „Trvalo to všetko možno aj dva roky – od prvých stretnutí s
-          producentom a bubeníkom Jakubom Krátkym, cez následný výber žalmových
-          spracovaní, až po distribúciu cédečiek.“ Album uviedli na festivale
-          Chvály v Hubošovciach v lete minulého roka. Jakub Krátky, Jozef
-          Šarišský, Pavol Hvišč a Marián Radvanský sú z hudobnej služby
-          prešovského kresťanského spoločenstva Marana Tha, kde sa s nimi Lenka
-          aj zoznámila. „Hrá sa nám spolu dobre, rozumieme si a viem, že sa na
-          nich môžem spoľahnúť.“ Vďaka spoločným priateľom spoznala trubkára
-          Adama Zákutného, ale aj Annu Krivdovú a Janku Janičinovú, ktoré sú v
-          jej projekte vokalistkami.
-        </p>
+          <li>
+            <a href="https://www.slovoplus.sk/spevacka-lenka-bednarova-mam-rada-zivot-hoci-aj-uprostred-tazkych-udalosti">
+            <h4>Slovo+</h4>
+              Speváčka Lenka Bednárová: Mám rada život, hoci aj uprostred ťažkých udalostí</a>
+          </li>
 
-        <h3>OBĽÚBENÉ ŽALMY</h3>
-
-        <p>
-          „Nemôžem povedať, že mám obľúbený žalm, ale mám žalmy, ktoré sa stali
-          mojimi obľúbenými v istých životných situáciách alebo obdobiach.“ Žalm
-          23 o Dobrom pastierovi si Lenka spievala v čase, keď prechádzala
-          rozchodom. V Žalme 124 Naša pomoc v mene Pánovom zase nachádzala oporu
-          pri štátniciach. Žalm 131 jej vždy pripomenie prednášku, na ktorej
-          prednášateľ opisoval Božiu lásku k človeku ako lásku matky vinúcej k
-          sebe milované, bezbranné dieťa.
-        </p>
-
-        <h3>VLASTNÁ TVORBA</h3>
-
-        <p>
-          Vášeň a láska k žalmom sprevádzajú Lenku celý život. Píše aj vlastné
-          texty, ktoré sú osobnou výpoveďou o Bohu a reakciami na životné
-          udalosti, ale ako hovorí, ľahšie sa vie stotožniť s už napísaným Božím
-          slovom – niektoré piesne vznikli spontánne počas modlitieb chvál.
-          Lenke sa doteraz podarilo hudobne spracovať približne 30 žalmov,
-          niektoré z nich si vyžadujú menšie úpravy, ale v zásuvke čakajú aj
-          také, ktoré treba hudobne dokončiť, či texty, ktoré má spracované len
-          pre osobnú modlitbu. Napriek tomu, že hudbu a spev miluje, o
-          profesionálnej kariére speváčky neuvažuje: „Hudbe a spevu sa venujem
-          aj v rámci doktorandského štúdia, pretože túžim rásť v talentoch,
-          ktoré mi Boh dal, ale nevnímam to ako svoje poslanie,“ vysvetľuje.
-          Lenkin talent a texty si napriek tomu získali srdcia mnohých. V roku
-          2017 dostala príležitosť zložiť hymnu pre kresťanský festival a
-          niekoľko autorských piesní sa v roku 2019 objavilo na albume
-          spoločenstva Marana Tha Drahocenný. Niektoré žalmy z CD boli súčasťou
-          diplomového koncertu v roku 2020. Aj keď zámerne na ďalšom albume
-          nepracuje, od vydania prvého sa Lenke podarilo nazbierať viacero
-          nových hudobných spracovaní. „Nato, aby vzniklo ďalšie cédečko, budem
-          musieť ešte nejakú časť svojej životnej cesty prejsť,“ s úsmevom
-          dodáva Lenka.
-        </p>
+          <li>
+            <a href="https://www.katolickenoviny.sk/mladym/category/mladym/article/lenka-zaodela-zalmy-do-noveho-satu.xhtml">
+            <h4>Katolícke noviny</h4>
+              Lenka zaodela žalmy do nového šatu - Mladým</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
