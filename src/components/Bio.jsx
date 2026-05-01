7a73b9af2b0fdbc48e321a38086f24d9e9eb1261
Robert Cernak <robert.cernak@cat.com>
2026-05-01 15:06:08 +0200

    bio - added breaks

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,30 +5,29 @@ export default function Bio() {
     <div id="bio" className="lighter-background section-padding">
       <div className="bio">
         <p>
-          Mgr. Lenka Radvanská, PhD. vyštudovala učiteľstvo hudobného umenia a biológie na Prešovskej univerzite v Prešove. Neskôr si svoje hudobné vedomosti rozšírila veľkým doktorátom z Didaktiky hudby na pôde Katedry hudby Katolíckej univerzity v Ružomberku. V oblasti súčasnej kresťanskej hudby sa podieľala na tvorbe hymny Campfestu Som nový (2017), bola súčasťou CD nosičov spoločenstva MaranaTha v Prešove, kde prispela aj autorskými piesňami Vydýchnem a Láska, láska (2019). Ako vokalistka spolupracovala s kresťanskými hudobnými kapelami Timothy, LCH live, s projektom Godzone, či hudobníkmi Dominikou Gurbaľovou a Petrom Křemenom. V roku 2020 spolu s kapelou nahrala debutový album Žalmy.
+          Mgr. Lenka Radvanská, PhD. vyštudovala učiteľstvo hudobného umenia a biológie na&nbsp;Prešovskej univerzite v&nbsp;Prešove. Neskôr si svoje hudobné vedomosti rozšírila veľkým doktorátom z&nbsp;Didaktiky hudby na&nbsp;pôde Katedry hudby Katolíckej univerzity v&nbsp;Ružomberku. V&nbsp;oblasti súčasnej kresťanskej hudby sa podieľala na&nbsp;tvorbe hymny Campfestu Som nový (2017), bola súčasťou CD nosičov spoločenstva MaranaTha v&nbsp;Prešove, kde prispela aj autorskými piesňami Vydýchnem a Láska, láska (2019). Ako vokalistka spolupracovala s&nbsp;kresťanskými hudobnými kapelami Timothy, LCH live, s&nbsp;projektom Godzone, či hudobníkmi Dominikou Gurbaľovou a Petrom Křemenom. V&nbsp;roku 2020 spolu s&nbsp;kapelou nahrala debutový album Žalmy.
         </p>
-
         <p>
-          Božie slovo a špeciálne Kniha žalmov má v mojom živote dôležité miesto. Verím, že cez Slovo sa nám Boh sám dáva spoznať osobným, živým spôsobom. V žalmoch sú ukryté všetky emócie, s ktorými sa človek môže stretnúť. Pomáhajú mi pochopiť, prijať, spracovať životné okolnosti a kráčať ďalej.
+          Božie slovo a špeciálne Kniha žalmov má v&nbsp;mojom živote dôležité miesto. Verím, že cez Slovo sa nám Boh sám dáva spoznať osobným, živým spôsobom. V&nbsp;žalmoch sú ukryté všetky emócie, s&nbsp;ktorými sa človek môže stretnúť. Pomáhajú mi pochopiť, prijať, spracovať životné okolnosti a kráčať ďalej.
         </p>
 
         <ul className="bio-links">
           <li>
             <a href="https://mojamuzika.dennikn.sk/rozhovory/tuzim-po-tom-aby-zalmove-texty-zazneli-v-kaviarnach-a-baroch-hovori-lenka-bednarova">
             <h4>Moja Muzika</h4>
-            Túžim po tom, aby žalmové texty zazneli v kaviarňach a baroch, hovorí Lenka Bednárová, ktorá žalmy zhudobňuje do jazzovej podoby</a>
+            Túžim po tom, aby žalmové texty zazneli v&nbsp;kaviarňach a baroch, hovorí Lenka Bednárová, ktorá žalmy zhudobňuje do jazzovej podoby</a>
           </li>
 
           <li>
             <a href="https://www.slovoplus.sk/spevacka-lenka-bednarova-mam-rada-zivot-hoci-aj-uprostred-tazkych-udalosti">
             <h4>Slovo+</h4>
-              Speváčka Lenka Bednárová: Mám rada život, hoci aj uprostred ťažkých udalostí</a>
+            Speváčka Lenka Bednárová: Mám rada život, hoci aj uprostred ťažkých udalostí</a>
           </li>
 
           <li>
             <a href="https://www.katolickenoviny.sk/mladym/category/mladym/article/lenka-zaodela-zalmy-do-noveho-satu.xhtml">
             <h4>Katolícke noviny</h4>
-              Lenka zaodela žalmy do nového šatu - Mladým</a>
+            Lenka zaodela žalmy do nového šatu</a>
           </li>
         </ul>
       </div>
