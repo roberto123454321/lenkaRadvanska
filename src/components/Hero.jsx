f0d6964db485b691b5e76adead2c61b762459129
Robert Cernak <robert.cernak@cat.com>
2026-05-16 12:45:59 +0200

    hero - added 3 dots navigator - added donio iframe - hero is now displayed under navbar, not behind it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import coverImage from "../../public/cover1.jpg";
 import coverImage2 from "../../public/cover2.jpg";
 import coverImage3 from "../../public/cover3.jpg";
@@ -8,27 +8,15 @@ export default function Hero() {
   const divs = [
     <a href="#video">
       <div className="article pos1 dark">
-        {/* <h3>Moja tvorba</h3> */}
-        {/* <div className="title-image"><img src="fonts/MojaTvorba.png" /></div> */}
         <p>Objavte videá s&nbsp;piesňami, v&nbsp;ktorých sa slová žalmistu menia na&nbsp;osobné výpovede.</p>
       </div>
       <img src={coverImage} />
     </a>,
 
-    <a href="https://donio.sk/">
-      <div className="article pos2 dark">
-        {/* <div className="title-image"><img src="fonts/CD.png" /></div> */}
-        {/* <h3>CD Ty rád</h3> */}
-        <p>V&nbsp;<strong>júni 2026</strong> vychádza môj druhý album.
-        Dala som mu názov <strong>Ty&nbsp;rád</strong> podľa&nbsp;rovnomennej piesne. <br/>
-        (Link na donio)</p>
-      </div>
-      <img src={coverImage2} />
-    </a>,
+    <iframe src="https://donio.sk/widget2/15784"></iframe>,
 
     <a href="#music">
       <div className="article pos3 dark">
-        {/* <h3>CD Žalmy</h3> */}
         <p>Vypočujte si môj debutový hudobný album.</p>
       </div>
       <img src={coverImage3} />
@@ -37,23 +25,45 @@ export default function Hero() {
 
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(true);
+  const intervalRef = useRef(null);
+
+  const goToSlide = (getNextIndex, reset = false) => {
+    setFade(false);
+
+    setTimeout(() => {
+      setIndex(getNextIndex);
+      setFade(true);
+
+      if (reset) resetTimer();
+    }, 500);
+  };
+
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      goToSlide(prev => (prev + 1) % divs.length);
+    }, 10000);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setFade(false);               // start fade-out
-      setTimeout(() => {
-        setIndex((prev) => (prev + 1) % divs.length);
-        setFade(true);              // fade-in new div
-      }, 500); // match fade-out duration
-    }, 10000);
-
-    return () => clearInterval(interval);
+    startTimer();
+    return () => clearInterval(intervalRef.current);
   }, []);
 
-  return (
-    <div className={`fade-wrapper ${fade ? "fade-in" : "fade-out"}`}>
+  return (  
+  <div className={`fade-wrapper ${fade ? "fade-in" : "fade-out"}`}>
       <div id="hero" className="hero">
         {divs[index]}
+        <div className={`dot-container ${index === 1 ? "dark-dots" : ""}`}>  
+          {divs.map((_, i) => (
+            <span
+              key={i}
+              className={`dot ${i === index ? "active" : ""}`}          
+              onClick={() => {
+                goToSlide(() => i, true);
+              }}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
