ec99359e097f111e9710e120704fdc61fd53e545
Robert Cernak <robert.cernak@cat.com>
2026-05-16 13:30:56 +0200

    contact - change email to instagram

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,23 @@
-import "./Contact.css"
+import "./Contact.css";
+import InstagramIcon from "../../public/instagram.svg";
 
 export default function Contact() {
   return (
     <div id="contact" className="section-padding darker-background">
       <div className="contact">
-        <h3>booking@radvanlenka.sk</h3>
+        <a
+          href="https://www.instagram.com/radvanlenka"
+          className="contact-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={InstagramIcon}
+            alt="Instagram"
+            className="instagram-icon"
+          />
+          <span>radvanlenka</span>
+        </a>
       </div>
     </div>
   );
